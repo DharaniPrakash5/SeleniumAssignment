571904bd4ee0d4d2390248c2de1712ad3e20d9a0
DharaniPrakash5 <90841408+DharaniPrakash5@users.noreply.github.com>
2025-11-10 09:04:11 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,27 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float Browser_version=91.0f;
+		String Developer_type="google";
+		Boolean Beta_type= false;
+		int Release_year=2008;
+		char Shortcut_key='C';
+		
+		
+		System.out.println("Developer type:"+Developer_type);
+		System.out.println("Browser Release year:"+Release_year);
+		System.out.println("Browser_version is:"+Browser_version);
+		System.out.println("Browser Beta version:"+Beta_type);
+		System.out.println("Browser Shortcut key is:"+Shortcut_key);
+		
+		
+		
+		
+ 
+	}
+
+}
---
+++ FindOddNumbers.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+public class FindOddNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int max_range= 170;
+		
+		for(int i=1; i<=max_range; i++)
+		{
+			if(i%2==1) 
+			{
+				System.out.println(i);
+			}
+			
+		}
+	}
+
+}
